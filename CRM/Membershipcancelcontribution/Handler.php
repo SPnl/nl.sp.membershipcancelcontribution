cb5d6284430951acfba064c41c69ea8f7745f0e7
Jaap Jansma <jaap@edeveloper.nl>
2017-07-27 14:23:58 +0200

    Fixed issue #1501: when ending a membership cancel the failed contributions.

--- CRM/Membershipcancelcontribution/Handler.php
+++ CRM/Membershipcancelcontribution/Handler.php
@@ -5,6 +5,7 @@ class CRM_Membershipcancelcontribution_Handler {
     public static function post($op, $objectName, $id, &$objectRef) {
         $cancelled_status_id = civicrm_api3('OptionValue', 'getvalue', array('return' => 'value', 'name' => 'Cancelled', 'option_group_name' => 'contribution_status'));
         $refunded_status_id = civicrm_api3('OptionValue', 'getvalue', array('return' => 'value', 'name' => 'Refunded', 'option_group_name' => 'contribution_status'));
+        $failed_status_id = civicrm_api3('OptionValue', 'getvalue', array('return' => 'value', 'name' => 'Failed', 'option_group_name' => 'contribution_status'));
         if ($objectName != 'Membership') {
             return;
         }
@@ -31,6 +32,22 @@ class CRM_Membershipcancelcontribution_Handler {
                 civicrm_api3('Contribution', 'Create', array('id' => $dao->id, 'contribution_status_id' => $cancelled_status_id));
             }
         }
+
+      //find contributions with status failed (4)
+      $sql = "SELECT c.id, c.contribution_status_id FROM `civicrm_contribution` c
+                INNER JOIN `civicrm_membership_payment` `mp` ON `c`.`id` = `mp`.`contribution_id`
+                where `mp`.`membership_id` = %1
+                and c.contribution_status_id = %2
+                  ";
+      $params = array();
+      $params[1] = array($id, 'Integer');
+      $params[2] = array($failed_status_id, 'Integer');
+      $dao = CRM_Core_DAO::executeQuery($sql, $params);
+      while ($dao->fetch()) {
+        $failedContribionParams = array('id' => $dao->id, 'contribution_status_id' => $cancelled_status_id);
+        CRM_Contribute_BAO_Contribution::create($failedContribionParams);
+        //civicrm_api3('Contribution', 'Create', array('id' => $dao->id, 'contribution_status_id' => $cancelled_status_id));
+      }
     }
 
 }
